504913fbbfc3086608b6f84e54306f97eb04a22c
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-01-18 19:37:21 -0500

    Added activity details request
    Saving to user activities to global dict because request.state data
    does not persist.
    
    * Added htmx
    * Additional style updates

--- fastapi-htmx/main.py
+++ fastapi-htmx/main.py
@@ -15,6 +15,9 @@ app.add_middleware(SessionMiddleware, secret_key=settings.session_secret_key, ma
 
 templates = Jinja2Templates(directory="templates")
 
+# TODO: Replace with better cache library???
+activity_cache = {}
+
 @app.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def index(request: Request):
     strava_user = request.session.get("strava_user")
@@ -22,10 +25,32 @@ async def index(request: Request):
     if not strava_user or strava.is_access_token_expired(strava_user.get("expires_at")):
         return templates.TemplateResponse("strava_login.html", {"request": request})
 
-    request.state.activities = strava.get_activities(strava_user.get('access_token'))
+    activities = strava.get_activities(strava_user.get('access_token'))
+    activity_cache[strava_user["athlete"]["id"]] = activities
     return templates.TemplateResponse(
         "content.html",
-        {"request": request}
+        {"request": request, "activities": activities, "activity": activities[0]}
+    )
+
+@app.get("/activity/{activity_id}", include_in_schema=False)
+async def get_activity(request: Request, activity_id: int):
+    strava_user = request.session.get("strava_user")
+    # TODO: Handle missing user
+    strava_user_id = strava_user["athlete"]["id"]
+    strava_user_activities = activity_cache.get(strava_user_id)
+    activity = None
+    if not strava_user_activities:
+        return Response(
+            content=f"Activity Id {activity_id} not found.",
+            status_code=status.HTTP_400_BAD_REQUEST
+        )
+    for item in strava_user_activities:
+        if item["id"] == activity_id:
+            activity = item
+            break
+    return templates.TemplateResponse(
+        "activity.html",
+        {"request": request, "activity": activity}
     )
 
 @app.get("/strava_authorize", include_in_schema=False)
