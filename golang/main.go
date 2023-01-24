17e282ae31b08584d331729fa6c3d6ee66fd3fc0
David Dominguez <352985+ddominguez@users.noreply.github.com>
2023-01-23 23:38:43 -0500

    Initial html rendering

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	stravaClientId     = os.Getenv("STRAVA_CLIENT_ID")
+	stravaClientSecret = os.Getenv("STRAVA_CLIENT_SECRET")
+)
+
+func indexHandler(response http.ResponseWriter, request *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/base.html"))
+	response.Header().Set("Content-Type", "text/html")
+	err := tmpl.Execute(response, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-    fmt.Println("I run marathons btw.")
+	http.HandleFunc("/", indexHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
